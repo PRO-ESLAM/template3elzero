3f37ac5adfcb20ba4a120eac8544f6415c76de51
PRO-ESLAM <106440929+PRO-ESLAM@users.noreply.github.com>
2022-12-22 15:03:29 -0800

    Elzero

---
+++ css/script.js
@@ -0,0 +1,113 @@
+// a JavaScript object...:
+var myObj = { "name":"John", "age":31, "city":"New York" };
+
+// ...converted into JSON:
+var myJSON = JSON.stringify(myObj);
+
+// send JSON:
+window.location = "demo_json.php?x=" + myJSON;
+const canvs = document.getElementById('my_canvas');
+const contet = canvas.getContext('2d');
+const imagData = context.getImageData(0, 0, canvas.width, canvas.height);
+const uin8ClampedArray = imageData.data;
+// a JavaScript object...:
+var myObj = { "name":"John", "age":31, "city":"New York" };
+
+// ...converted into JSON:
+var myJSON = JSON.stringify(myObj);
+
+// send JSON:
+window.location = "demo_json.php?x=" + myJSON;
+const can   = document.getElementById('my_canvas');
+const conte   = canvas.getContext('2d');
+const imageDat  = context.getImageData(0, 0, canvas.width, canvas.height);
+const uint8ClampedArra = imageData.data;// a JavaScript object...:
+var myObj = { "name":"John", "age":31, "city":"New York" };
+
+// ...converted into JSON:
+var myJSON = JSON.stringify(myObj);
+
+// send JSON:
+window.location = "demo_json.php?x=" + myJSON;
+const  anvas = document.getElementById('my_canvas');
+const  ontext = canvas.getContext('2d');
+const iageData = context.getImageData(0, 0, canvas.width, canvas.height);
+const  t8ClampedArray = imageData.data;// a JavaScript object...:
+var myObj = { "name":"John", "age":31, "city":"New York" };
+
+// ...converted into JSON:
+var myJSON = JSON.stringify(myObj);
+
+// send JSON:
+window.location = "demo_json.php?x=" + myJSON;
+const  vas = document.getElementById('my_canvas');
+const  ext = canvas.getContext('2d');
+const   Data = context.getImageData(0, 0, canvas.width, canvas.height);
+const   lampedArray = imageData.data;// a JavaScript object...:
+var myObj = { "name":"John", "age":31, "city":"New York" };
+
+// ...converted into JSON:
+var myJSON = JSON.stringify(myObj);
+
+// send JSON:
+window.location = "demo_json.php?x=" + myJSON;
+// const canv s = document.getElementById('my_canvas');
+// const context = canvas.getContext('2d');
+// const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+// const uint8ClampedArray = imageData.data;// a JavaScript object...:
+// var myObj = { "name":"John", "age":31, "city":"New York" };
+
+// ...converted into JSON:
+var myJSON = JSON.stringify(myObj);
+
+// send JSON:
+// window.location = "demo_json.php?x=" + myJSON;
+// const canvas = document.getElementById('my_canvas');
+// const context = canvas.getContext('2d');
+// const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+// const uint8ClampedArray = imageData.data;// a JavaScript object...:
+// var myObj = { "name":"John", "age":31, "city":"New York" };
+
+// ...converted into JSON:
+var myJSON = JSON.stringify(myObj);
+
+// send JSON:
+// window.location = "demo_json.php?x=" + myJSON;
+// const canvas = document.getElementById('my_canvas');
+// const context = canvas.getContext('2d');
+// const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+// const uint8ClampedArray = imageData.data;// a JavaScript object...:
+// var myObj = { "name":"John", "age":31, "city":"New York" };
+
+// ...converted into JSON:
+var myJSON = JSON.stringify(myObj);
+
+
+window.location = "demo_json.php?x=" + myJSON;
+const canvyas = document.getElementById('my_canvas');
+const cont4e4xt = canvas.getContext('2d');
+const image2Data = context.getImageData(0, 0, canvas.width, canvas.height);
+const uint8C4lamperdArray = imageData.data;// a JavaScript object...:
+var myObj = { "name":"John", "age":31, "city":"New York" };
+
+ 
+var myJSON = JSON.stringify(myObj);
+
+// send JSON:
+window.location = "demo_json.php?x=" + myJSON;
+const canv4as = document.getElementById('my_canvas');
+const cont4ext = canvas.getContext('2d');
+const imag4eData = context.getImageData(0, 0, canvas.width, canvas.height);
+const uint8C4lampedArray = imageData.data;// a JavaScript object...:
+var myObj = { "name":"John", "age":31, "city":"New York" };
+
+// ...converted into JSON:
+var myJSON = JSON.stringify(myObj);
+
+
+ 
+window.location = "demo_json.php?x=" + myJSON;
+const canvas = document.getElementById('my_canvas');
+const context = canvas.getContext('2d');
+const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+const uint8ClampedArray = imageData.data;
